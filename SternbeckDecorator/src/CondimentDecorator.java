7961989e58619d5e293ad8590a3352ae08dbee47
sscholz0 <sscholz@student.tgm.ac.at>
2016-06-12 13:46:09 +0200

    delete unnecessery files

--- SternbeckDecorator/src/CondimentDecorator.java
+++
@@ -1,9 +0,0 @@
-
-public abstract class CondimentDecorator extends Beverage {
-	private Beverage wrapped;
-	
-	public String getDescription(){
-		return (this.getDescription()+"+ "+wrapped.getDescription());
-	}
-
-}
